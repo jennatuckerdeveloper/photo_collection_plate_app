8071096e3a1ec9cb94d8ebca850b9270cf30f941
JennaMTucker <jenna.m.tucker@gmail.com>
2018-01-18 13:33:45 -0800

    LogInPage html change

--- src/LogInPage.js
+++ src/LogInPage.js
@@ -6,9 +6,9 @@ export default class LogInPage extends Component {
       <div id='logIn' >
         <form id='logInInputs' onSubmit={this.props.logIn}>
           <label htmlFor='email'>Email</label>
-          <input name='email' id='email' type='text' placeholder='Enter text' onChange={this.props.handleChange} />
-          <label htmlFor='password' type='text'>Password</label>
-          <input name='password' id='password' type='text' placeholder='Enter password' onChange={this.props.handleChange} />
+          <input name='email' id='email' type='email' placeholder='Enter text' onChange={this.props.handleChange} />
+          <label htmlFor='password'>Password</label>
+          <input name='password' id='password' type='password' placeholder='Enter password' onChange={this.props.handleChange} />
           <button onClick={this.logIn}>Log In</button>
         </form>
       </div>
