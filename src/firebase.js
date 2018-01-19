517510512c857cb18a6ad67e7b23a50aaec267e2
JennaMTucker <jenna.m.tucker@gmail.com>
2018-01-19 11:26:37 -0800

    adding env

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase'
+require('dotenv').config()
 const config = {
-  apiKey: $apiKey,
-  authDomain: $authDomain,
-  databaseURL: $databaseURL,
-  projectId: $projectId,
-  storageBucket: $storageBucket,
-  messagingSenderId: $messagingSenderId
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
 }
 
 firebase.initializeApp(config)
