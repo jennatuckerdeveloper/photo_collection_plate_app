9cdc0d7f4b57ea4bad4103deaa843368d63a2d98
Peter Banka <pbanka@fastly.com>
2018-02-14 20:19:30 -0800

    Adding a first example of how to pull async actions out of Main.js

--- src/actions.js
+++ src/actions.js
@@ -1,12 +1,14 @@
-export const TO_LOG_IN = 'TO_LOG_IN'
-export const TO_SIGN_UP = 'TO_SIGN_UP'
-export const TO_PROFILES = 'TO_PROFILES'
 export const SET_USER = 'SET_USER'
 export const NEW_USER_INFO = 'NEW_USER_INFO'
 export const ALL_PROFILES = 'ALL_PROFILES'
 export const PAGE_PROFILES = 'PAGE_PROFILES'
 export const CHANGE_PAGE = 'CHANGE_PAGE'
+export const LOG_IN = 'LOG_IN'
+export const REPORT_ERROR = 'REPORT_ERROR'
+
 // export const REPORT_ERROR = 'REPORT_ERROR'
+
+export const logIn = () => ({ type: LOG_IN })
 
 export const setAUser = (UID) => {
   return {
@@ -44,10 +46,10 @@ export const changePage = (newPage) => {
   }
 }
 
-// export const reportError = (error) => {
-//   return {
-//     type: REPORT_ERROR,
-//     errorCode: error.code,
-//     errorMessage: error.message
-//   }
-// }
+export const reportError = (error) => {
+  return {
+    type: REPORT_ERROR,
+    errorCode: error.code,
+    errorMessage: error.message
+  }
+}
